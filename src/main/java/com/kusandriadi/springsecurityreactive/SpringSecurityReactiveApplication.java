4c719498a7dfa450b731fa56e3369a7222163f67
kusand <me@kusandriadi.com>
2021-08-01 17:31:48 +0700

    adding pom.xml and src template

---
+++ src/main/java/com/kusandriadi/springsecurityreactive/SpringSecurityReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.kusandriadi.springsecurityreactive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityReactiveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kusandriadi/springsecurityreactive/SpringSecurityReactiveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kusandriadi.springsecurityreactive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
